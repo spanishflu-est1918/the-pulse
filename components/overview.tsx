5664e6b722aebceed40ebfec37b18256c4c1842e
SPANISH FLU <mrflu1918@proton.me>
2025-03-01 19:01:20 +0100

    the pulse

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,6 +1,4 @@
 import { motion } from "framer-motion";
-import { MessageIcon } from "./icons";
-import Image from "next/image";
 
 export const Overview = () => {
   return (
@@ -14,13 +12,23 @@ export const Overview = () => {
     >
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
         <div className="flex flex-col items-center gap-4">
-          <Image
-            src="/images/pulse.jpg"
-            alt="Logo"
-            width={200}
-            height={200}
-            className="rounded-md"
-          />
+            <motion.div
+              className="size-8 bg-foreground rounded-full"
+              animate={{
+                scale: [1, 1.5, 1],
+                opacity: [0.7, 0.3, 0.7],
+                boxShadow: [
+                  "0 0 0 0 rgba(59, 130, 246, 0.7)",
+                  "0 0 0 20px rgba(59, 130, 246, 0)",
+                  "0 0 0 0 rgba(59, 130, 246, 0.7)"
+                ]
+              }}
+              transition={{
+                duration: 2,
+                repeat: Infinity,
+                ease: "easeInOut"
+              }}
+            />
         </div>
         <h1 className="text-3xl font-bold">The Pulse</h1>
       </div>
