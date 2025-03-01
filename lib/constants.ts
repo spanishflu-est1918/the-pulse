6bb89a32d4d392f3e9448b13d7b0344d68f3bb3a
SPANISH FLU <mrflu1918@proton.me>
2025-03-01 21:32:32 +0100

    RTT

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,6 @@
-export const VALID_INVITE_CODES = [
-  'ACYBORG',
-  'CYBERYOGIN',
-];
+export const VALID_INVITE_CODES = ["ACYBORG", "CYBERYOGIN", "RTT"];
 
 export const INVITE_CODE_ERROR_MESSAGES = {
-  INVALID: 'Invalid invite code',
-  REQUIRED: 'Invite code is required',
-}; 
+  INVALID: "Invalid invite code",
+  REQUIRED: "Invite code is required",
+};
